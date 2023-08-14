329ac201e3d082c3413d25792f945f1d5caf15a1
phaks323 <phaks323@gmail.com>
2023-08-14 13:53:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-st.markdown(full_response)
+st.markdown(m["content"])
