8db4357788f5f3c17426ac95b392d0e7e0a2ba64
phaks323 <phaks323@gmail.com>
2023-08-15 09:45:30 +0200

    Update strseamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-    vv = full_response.partition(":")[2].split('In this code')[0]
+    vv = full_response.split('In this code')[0]
     st.markdown(vv)
 
     
