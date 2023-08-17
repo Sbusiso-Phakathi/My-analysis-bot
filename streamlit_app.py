added013465399034682013a3eb6d31b74a23dfa
phaks323 <phaks323@gmail.com>
2023-08-17 08:59:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,25 @@ import nbformat
 import streamlit as st
 import subprocess
 import sys
-
+from io import StringIO
+import pandas as pd
 
 with st.sidebar:
     st.title('🤖💬 SmatAnalysis Appz')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
+    openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+    user = st.text_input('Enter Email:')
+    if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+        st.warning('Please enter your credentials!', icon='⚠️')
     else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-            subprocess.run([f"{sys.executable}", "script.py"])
+        st.success('Proceed to entering your prompt message!', icon='👉')
 
+
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        df.to_csv(user + '.csv')
+
+            
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -46,5 +50,27 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
         vv = full_response.partition(":")[2].split('print("# Done")')[0]
-        st.markdown(vv)
+   
+    st.markdown(vv)
+with open('script.py','r') as f:
+
+	newlines = []
+	i = 0
+	for line in f:
+		if "plt.show()" in line:
+			newlines.append(line.replace('plt.show()', "plt.savefig('"+ str(i) +".png') \nplt.close()"))
+			i = i + 1
+		else:
+			newlines.append(line)
+with open('script.py', 'w') as f:
+                        for line in newlines:
+                            f.write(line)
+subprocess.run([f"{sys.executable}", "script.py"])
+
+import cv2
+import glob
+for img in glob.glob("*.png"):
+    cv_img = cv2.imread(img)
+    st.image(cv_img, caption='Sunrise by the mountains')
+
 
